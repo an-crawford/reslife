356df58f605c1dc4ffb51726f054cd7d8f1389fd
Zekai <57110403+Zekai-Wang@users.noreply.github.com>
2023-06-12 21:12:11 -0400

    Create gengamma.orig.R

---
+++ R/gengamma.orig.R
@@ -0,0 +1,79 @@
+#######################
+#### GenGamma Orig ####
+#######################
+
+####Written by Zekai Wang ####
+
+library('flexsurv')
+
+upper_incomplete_gamma <- function(x,a) {
+  #return (incgam(x,a))
+  return (gamma(a) * pgamma(x, a, 1, lower = FALSE))
+}
+
+
+gengamma.orig = function(fsroutput, x, p=.5, type = 'all', newdata = data.frame()){
+  if (length(newdata)!=0){
+    if (length(newdata) == 1){
+      stopifnot(fsroutput$covdata$covnames == colnames(newdata))
+    }
+    else{
+      names = fsroutput$covdata$covnames
+      newdata= newdata[,c(names)]
+      print(newdata)
+    }
+  }
+  b = as.numeric(exp(fsroutput$coefficients[1]))
+  k = as.numeric(exp(fsroutput$coefficients[3]))
+  if (length(newdata) == 0){
+    if (fsroutput$ncovs == 0) {
+      a = as.numeric(exp(fsroutput$coefficients[2]))
+    }
+    else{
+      s = fsroutput$coefficients
+      a = exp(as.matrix(fsroutput$data$mml$scale) %*% as.numeric(s[-c(1,3)]))
+    }
+  }
+  else{
+    if (fsroutput$ncovs == 0) {
+      a = as.numeric(exp(fsroutput$coefficients[2]))
+    }
+    else{
+      X<-model.matrix( ~ ., data = newdata)
+      s = fsroutput$coefficients
+      sa = s[2]
+      sb = s[-c(1,2,3)]
+      sb = sb[colnames(X)]
+      sb = sb[!is.na(sb)]
+      sc = append(sa,sb)
+      if (length(sc) != ncol(X)){
+        print('Incorrect Level Entered')
+        error = 1
+        stopifnot(error = 0)
+      }
+      a = exp(as.matrix(X) %*% as.numeric(sc))
+    }
+  }
+  
+  sx = pgengamma.orig(x, shape = b, scale = a, k = k, lower.tail = FALSE)
+  mx = a * upper_incomplete_gamma((x/a)^b, k + 1/b)/upper_incomplete_gamma((x/a)^b, k) - x
+  px = function(p){
+    pc = (1-p)*sx
+    px = as.numeric(qgengamma.orig(pc, shape = b, scale = a, k = k, lower.tail = FALSE) - x)
+  }
+  if (type=='mean'){
+    return(list('mean'= mx))
+  }
+  if (type== 'median'){
+    return(list('median'= px(.5)))
+  }
+  if (type == 'percentile'){
+    return(list('percentile'= px(p)))
+  }
+  if (type == 'all'){
+    return(list("mean" = mx, 'median' = px(.5), 'percentile' = px(p)))
+  }
+  else{
+    return('invalid type')
+  }
+}
