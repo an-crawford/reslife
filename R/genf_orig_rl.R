59dbedb5d28929449596b0657ec1f97c68ccb73d
Ka Lok Lee <112717156+kaloklee@users.noreply.github.com>
2023-09-15 13:43:13 -0400

    Update genf_orig_rl.R

--- R/genf_orig_rl.R
+++ R/genf_orig_rl.R
@@ -276,10 +276,6 @@ genF_orig_rl = function(fsroutput, x, p=.5, type = 'all', newdata = data.frame()
     return(c(mx))
 
   }
-    
-    
-    
-  }
   
   
   
@@ -309,9 +305,9 @@ genF_orig_rl = function(fsroutput, x, p=.5, type = 'all', newdata = data.frame()
   
   if (type == 'all'){
     
-  if (pmin(c(mx))< 0){
-       message("Estimated parameters produce an undefined mean.")
-   }
+    if(pmin(c(mx))< 0){
+         message("Estimated parameters produce an undefined mean.")
+     }
     
     return(data.frame(mean = mx, median = px(.5), percentile = px(p)))
     
